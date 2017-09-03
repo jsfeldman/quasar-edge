6304d45de80f1d882a2e0af379eb92df57a313bb
Razvan Stoenescu <razvan.stoenescu@gmail.com>
2017-09-03 15:12:34 +0300

    publish: chore: Bump version to 0.14.4
    generated from commit 776d735a7268f7c2a48acc7dc98bde355b448973

--- dist/quasar.esm.js
+++ dist/quasar.esm.js
@@ -1,5 +1,5 @@
 /*!
- * Quasar Framework v0.14.3
+ * Quasar Framework v0.14.4
  * (c) 2016-present Razvan Stoenescu
  * Released under the MIT License.
  */
@@ -312,7 +312,7 @@ var theme = Object.freeze({
 	get current () { return current; }
 });
 
-var version = "0.14.3";
+var version = "0.14.4";
 
 var Vue;
 
--- dist/quasar.ie.js
+++ dist/quasar.ie.js
@@ -1,5 +1,5 @@
 /*!
- * Quasar Framework v0.14.3
+ * Quasar Framework v0.14.4
  * (c) 2016-present Razvan Stoenescu
  * Released under the MIT License.
  */
